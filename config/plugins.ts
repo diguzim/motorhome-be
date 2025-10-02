33a6f4016788c06e8feb1476675edbe17305d29e
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2025-10-01 22:06:26 -0300

    Only using cloudinary in production

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,16 +1,23 @@
 module.exports = ({ env }) => ({
   upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
+    config:
+      env("NODE_ENV") === "production"
+        ? {
+            // Cloudinary configuration for production
+            provider: "cloudinary",
+            providerOptions: {
+              cloud_name: env("CLOUDINARY_NAME"),
+              api_key: env("CLOUDINARY_KEY"),
+              api_secret: env("CLOUDINARY_SECRET"),
+            },
+            actionOptions: {
+              upload: {},
+              delete: {},
+            },
+          }
+        : {
+            // Default local storage for development
+            sizeLimit: 100 * 1024 * 1024, // 100MB
+          },
   },
 });
